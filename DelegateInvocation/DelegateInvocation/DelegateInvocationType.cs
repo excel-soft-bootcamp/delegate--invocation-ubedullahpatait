a8f6e37607b88282fa15226958b7bec0ac425bcd
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-07-10 12:45:48 +0530

    Add files via upload

---
+++ DelegateInvocation/DelegateInvocation/DelegateInvocationType.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateInvocation
+{
+    class DelegateInvocationType
+    {
+
+        static double Calculate(int x, double y) 
+        { 
+            return x + y; 
+        }
+
+        void Update(int newNumber) { System.Console.WriteLine(newNumber); }
+
+        void Delete(string key) { }
+
+        void obj()
+        {
+        }
+
+        void obj2(string data) 
+        { 
+        }
+
+        int obj3(int x)
+        {
+            return x;
+        }
+
+        bool obj4(string name , string adress)
+        {
+          
+
+        }
+
+        static void FunctionsAsAnArguments(Action obj, Action<string> obj2, Func<int, int> obj3, Func<string, string, bool> obj4)
+        {
+
+            //Invoke Delegate objects
+
+
+        }
+
+    }
+}
---
+++ DelegateInvocation/DelegateInvocation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateInvocation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateInvocationType DelegateRef = new DelegateInvocationType();
+
+            Func<int, double, double> calculateptr = new Func<int, double, double>(DelegateInvocationType.Calculate);
+
+                Action<int> updateptr = new Action<int>(DelegateRef.update);
+
+            Action<string> Deleteptr = new Action<string>(DelegateRef.Delete);
+
+           FunctionArgument()
+
+
+
+        }
+    }
+}
